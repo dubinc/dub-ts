b8018bc0aa7e7832dd4349a381ea8d19e8310163
Kiran K <mailtokirankk@gmail.com>
2024-04-10 11:34:31 +0530

    Move speakeasy to the root

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -54,7 +54,7 @@ export function serverURLFromOptions(options: SDKOptions): URL | null {
 export const SDK_METADATA = Object.freeze({
     language: "typescript",
     openapiDocVersion: "0.0.1",
-    sdkVersion: "0.18.3",
+    sdkVersion: "0.19.0",
     genVersion: "2.301.0",
-    userAgent: "speakeasy-sdk/typescript 0.18.3 2.301.0 0.0.1 dub",
+    userAgent: "speakeasy-sdk/typescript 0.19.0 2.301.0 0.0.1 dub",
 });
