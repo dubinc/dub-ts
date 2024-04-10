4a41f1234e1b5483c54ba6a8c826988469d7df70
Kiran K <kiran@dub.co>
2024-04-10 14:26:28 +0530

    Remove stainless dep  (#59)
    * Remove stainless and its dep
    
    * Move speakeasy to the root

--- src/resources/projects/index.ts
+++
@@ -1,4 +0,0 @@
-// File generated from our OpenAPI spec by Stainless.
-
-export { ProjectRetrieveResponse, ProjectListResponse, ProjectRetrieveParams, Projects } from './projects';
-export { Tags } from './tags';
